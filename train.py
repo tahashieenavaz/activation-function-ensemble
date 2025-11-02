2c72345377ead8b0b48753203a7a54722db92a98
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-02 17:57:57 +0100

    debug

--- train.py
+++ train.py
@@ -1,13 +1,12 @@
 from homa import settings
-
-# from homa.models import StochasticResnet
+from homa.models import StochasticResnet
 
 from datasets import BG
 from datasets import AugmentedDataset
 
 print(BG())
 for i in range(settings("size")):
-    # model = StochasticResnet()
+    model = StochasticResnet(3)
 
     for epoch in range(settings("epochs")):
         pass
