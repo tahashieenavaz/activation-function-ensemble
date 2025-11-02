8a55b04a1ba8d21fbb8226453dd3202f8734f93a
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-02 17:30:12 +0100

    debug comments

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 from homa import settings
-from homa.models import StochasticResnet
+
+# from homa.models import StochasticResnet
+from datasets import BG
 
 
 for i in range(settings("size")):
-    model = StochasticResnet()
+    # model = StochasticResnet()
 
     for epoch in range(settings("epochs")):
         pass
